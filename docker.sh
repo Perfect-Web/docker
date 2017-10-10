39ca3aa67ec7fc57eb4578f7d237753409e5c891
Alin Jurj <office@perfectweb.ro>
2017-10-10 09:24:29 +0300

    enable xmr-stak-amd hash reporting

--- docker.sh
+++ docker.sh
@@ -1,1 +1,2 @@
 docker run -dit -p 80:80 -p 9000:9000 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v $HOME/.ssh:/root/.ssh -v $HOME/Web/html:/var/www/html --name=development perfectweb/development
+
