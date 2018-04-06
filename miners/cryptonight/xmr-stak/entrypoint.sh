c96e829291f1898d53a003d5cbc3bf5b6b1f39df
Alin Jurj <office@perfectweb.ro>
2018-04-06 17:12:54 +0300

    changes

--- miners/cryptonight/xmr-stak/entrypoint.sh
+++ miners/cryptonight/xmr-stak/entrypoint.sh
@@ -6,7 +6,9 @@ sed -i 's/{PASSWORD}/'"$PASSWORD"'/g' pools.txt
 sed -i 's/{POOLHOSTNAME}/'"$POOLHOSTNAME"'/g' pools.txt
 sed -i 's/{PORT}/'"$PORT"'/g' pools.txt
 sed -i 's/{WALLET}/'"$WALLET"'/g' pools.txt
+sed -i 's/{WORKER}/'"$WORKER"'/g' pools.txt
+sed -i 's/{CURRENCY}/'"$CURRENCY"'/g' pools.txt
 
 touch output.log
-./xmr-stak -c pools.txt "$@" &> /dev/null &
+./xmr-stak -c config.txt -C pools.txt "$@" &> /dev/null &
 tail -f output.log
