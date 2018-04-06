61b56d761323d3a047d47116d8bf4f2a7b2f0553
Alin Jurj <office@perfectweb.ro>
2018-04-06 12:58:49 +0300

    update xmr stack to 2.4.2

--- miners/cryptonight/xmr-stak/entrypoint.sh
+++ miners/cryptonight/xmr-stak/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-cp config.tpl config.txt
-
-sed -i 's/{NICEHASH}/'"$NICEHASH"'/g' config.txt
-sed -i 's/{SECURE}/'"$SECURE"'/g' config.txt
-sed -i 's/{PASSWORD}/'"$PASSWORD"'/g' config.txt
-sed -i 's/{POOLHOSTNAME}/'"$POOLHOSTNAME"'/g' config.txt
-sed -i 's/{PORT}/'"$PORT"'/g' config.txt
-sed -i 's/{WALLET}/'"$WALLET"'/g' config.txt
+sed -i 's/{NICEHASH}/'"$NICEHASH"'/g' pools.txt
+sed -i 's/{SECURE}/'"$SECURE"'/g' pools.txt
+sed -i 's/{PASSWORD}/'"$PASSWORD"'/g' pools.txt
+sed -i 's/{POOLHOSTNAME}/'"$POOLHOSTNAME"'/g' pools.txt
+sed -i 's/{PORT}/'"$PORT"'/g' pools.txt
+sed -i 's/{WALLET}/'"$WALLET"'/g' pools.txt
 
 touch output.log
-./xmr-stak -c config.txt "$@" &> /dev/null &
+./xmr-stak -c pools.txt "$@" &> /dev/null &
 tail -f output.log
