b114a63c765d3bfa4efb6acf0df4968be225c961
Alin Jurj <office@perfectweb.ro>
2018-07-19 14:37:53 +0300

    update to xmr-stak-v2.4.7

--- miners/cryptonight/xmr-stak/entrypoint.sh
+++ miners/cryptonight/xmr-stak/entrypoint.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 cp config.tpl config.txt
+cp pools.tpl pools.txt
 
-sed -i 's/{NICEHASH}/'"$NICEHASH"'/g' config.txt
-sed -i 's/{SECURE}/'"$SECURE"'/g' config.txt
-sed -i 's/{PASSWORD}/'"$PASSWORD"'/g' config.txt
-sed -i 's/{POOLHOSTNAME}/'"$POOLHOSTNAME"'/g' config.txt
-sed -i 's/{PORT}/'"$PORT"'/g' config.txt
-sed -i 's/{WALLET}/'"$WALLET"'/g' config.txt
+sed -i 's/{NICEHASH}/'"$NICEHASH"'/g' pools.txt
+sed -i 's/{SECURE}/'"$SECURE"'/g' pools.txt
+sed -i 's/{PASSWORD}/'"$PASSWORD"'/g' pools.txt
+sed -i 's/{POOLHOSTNAME}/'"$POOLHOSTNAME"'/g' pools.txt
+sed -i 's/{PORT}/'"$PORT"'/g' pools.txt
+sed -i 's/{WALLET}/'"$WALLET"'/g' pools.txt
+sed -i 's/{CURRENCY}/'"$CURRENCY"'/g' pools.txt
+sed -i 's/{WORKER}/'"$WORKER"'/g' pools.txt
 
 touch output.log
 ./xmr-stak -c config.txt "$@" &> /dev/null &
